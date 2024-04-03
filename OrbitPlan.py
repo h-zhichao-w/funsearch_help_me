8e67a7888472adf577a34f6db5ccc90784956c16
Hansen <hansen.wzc@gmail.com>
2024-04-03 14:21:51 +0300

    完成了将栅格数据中的点连成线的功能

---
+++ OrbitPlan.py
@@ -0,0 +1,31 @@
+import scipy.io as sio
+import numpy as np
+from Point2Strip import point2strip
+
+taskFileName = "中国区域"
+gridFileName = "任务栅格数据\\工作模式2_8个波位\\"
+resultFileName = "贪婪规划结果\\工作模式2_8个波位\\"    # 输出位置
+
+# 加载栅格数据
+gridData = sio.loadmat(gridFileName + taskFileName + '.mat')
+gridData = np.array(gridData['gridData'])
+
+print(type(gridData))
+
+# 约束条件
+openNumMax = 6
+openOrbitTimeMax = 500
+surveyOrbitNumMax = 12
+surveyOrbitTimeMax = 300
+openTimeMax = 180
+openTimeMin = 10
+openInter = 60
+surveyOpenNumMax = 4
+surveyOpenTimeMax = 150
+surveyTimeMax = 60
+surveyTimeMin = 10
+surveyInterSame = 26
+surveyInterDiff = 52
+
+gridDataLX = point2strip(gridData)
+
---
+++ Point2Strip.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def point2strip(grid_data):
+    """
+    Args:
+        grid_data: 2D array, 其大小为 orbitNum 行 waveNum 列，其中 orbitNum 表示轨道数，waveNum 表示波位数。
+    
+    Returns:
+        gridDataLX: 经过处理后的栅格数据，仍为二维 cell 数组，大小与输入参数 gridData 相同，LX for 连续。
+    """
+
+    orbit_num, wave_num = grid_data.shape[0], grid_data.shape[1]
+    gridDataLX = np.zeros((orbit_num, wave_num), dtype=object)
+
+    for orbit in range(orbit_num):
+        for wave in range(wave_num):
+            if not grid_data[orbit, wave].size:
+                continue
+            grid_cell = grid_data[orbit, wave]
+            pst = 0
+            pend = 1
+            LX = []
+            while True:
+                if pend == grid_cell.shape[0]:
+                    LX.append([grid_cell[pst, 0], grid_cell[pend - 1, 0], grid_cell[pst, 2]])
+                    break
+                if grid_cell[pend, 0] - grid_cell[pend - 1, 0] == 1:
+                    pend += 1
+                    continue
+                if grid_cell[pend, 0] - grid_cell[pend - 1, 0] > 1:
+                    LX.append([grid_cell[pst, 0], grid_cell[pend - 1, 0], grid_cell[pst, 2]])
+                    pst = pend
+                    pend += 1
+                    continue
+            gridDataLX[orbit, wave] = LX
+
+    return gridDataLX
