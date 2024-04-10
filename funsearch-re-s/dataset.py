8614d886ab701109f7c4e518f35cedbeaad1f89a
Hansen <hansen.wzc@gmail.com>
2024-04-10 09:25:51 +0300

    补充了evaluation

---
+++ funsearch-re-s/dataset.py
@@ -0,0 +1,17 @@
+dataset = {
+    'task_name': "中国区域",
+    'grid_path': "D:\\OneDrive - sjtu.edu.cn\\Bachelor Thesis\\MinimumTime\\任务栅格数据\\工作模式2_8个波位\\",
+    'boot_num_max': 6,
+    'boot_orbit_time_max': 500,
+    'survey_orbit_num_max': 12,
+    'survey_orbit_time_max': 300,
+    'boot_time_max': 180,
+    'boot_time_min': 10,
+    'boot_inter': 60,
+    'survey_boot_num_max': 4,
+    'survey_boot_time_max': 150,
+    'survey_time_max': 60,
+    'survey_time_min': 10,
+    'survey_inter_same': 26,
+    'survey_inter_diff': 52
+}
